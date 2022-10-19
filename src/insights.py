13b4ceffb361eab533d80c52534b2a5f0c4c251b
Lalá Nametala <lalanametala@gmail.com>
2022-10-19 20:50:05 -0300

    :sparkles: feat(insights): implements the filter_by_job_type function

--- src/insights.py
+++ src/insights.py
@@ -12,21 +12,12 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    filtered_jobs = []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            filtered_jobs.append(job)
+    return filtered_jobs
 
 
 def get_unique_industries(path):
