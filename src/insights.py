7801c50a8db4c0e6217afe6e14c0bd6a4a052281
Lalá Nametala <lalanametala@gmail.com>
2022-10-19 20:51:18 -0300

    :sparkles: feat(insights): implements the filter_by_industry function

--- src/insights.py
+++ src/insights.py
@@ -31,21 +31,12 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
+    filtered_jobs = []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    for job in jobs:
+        if job["industry"] == industry:
+            filtered_jobs.append(job)
+    return filtered_jobs
 
 
 def get_max_salary(path):
