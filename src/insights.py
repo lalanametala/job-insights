7a9a7919f9ac78eed141bcb1abd5a16bf6c5453b
Lalá Nametala <lalanametala@gmail.com>
2022-10-19 19:29:23 -0300

    :sparkles: feat(insights): implements the get_unique_industries function

--- src/insights.py
+++ src/insights.py
@@ -31,21 +31,13 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    all_jobs = read(path)
+    unique_industries = set()
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    for job in all_jobs:
+        if job["industry"] != '':
+            unique_industries.add(job["industry"])
+    return unique_industries
 
 
 def filter_by_industry(jobs, industry):
