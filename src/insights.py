73ac8ee27a4a120abed22974d279dbf714eb9919
Lalá Nametala <lalanametala@gmail.com>
2022-10-19 19:35:59 -0300

    :sparkles: feat(insights): implements the get_max_salary function

--- src/insights.py
+++ src/insights.py
@@ -59,21 +59,12 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    all_jobs = read(path)
+    max_salaries = []
+    for job in all_jobs:
+        if job["max_salary"].isdigit():
+            max_salaries.append(int(job["max_salary"]))
+    return max(max_salaries)
 
 
 def get_min_salary(path):
