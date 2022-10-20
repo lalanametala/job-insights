ca7577de7f38a8ebf5fa83cce253b7ec6acefc7c
Lalá Nametala <lalanametala@gmail.com>
2022-10-19 21:48:31 -0300

    :test_tube: test(brazilian): implements the test_brazilian_jobs function

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    dict_english = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for job in dict_english:
+        assert job.get("titulo") is None
+        assert job.get("salario") is None
+        assert job.get("tipo") is None
+        assert job.get("title") is not None
+        assert job.get("salary") is not None
+        assert job.get("type") is not None
