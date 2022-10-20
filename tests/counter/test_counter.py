4f196c263a3bf23f02569c7e3e5d2b4d49c7672d
Lalá Nametala <lalanametala@gmail.com>
2022-10-19 21:34:30 -0300

    :test_tube: test(counter): implements the test_counter function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,11 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    WORDS = ["python", "PYTHON", "javascript", "JAVAscript"]
+    PATH = "src/jobs.csv"
+    expected_results = [1639, 1639, 122, 122]
+
+    for i in range(len(WORDS)):
+        count_word = count_ocurrences(PATH, WORDS[i])
+        assert count_word == expected_results[i]
