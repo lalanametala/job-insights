bd4539755f34254b47d3e841b1dd7d5c6edf562c
Lalá Nametala <lalanametala@gmail.com>
2022-10-19 21:13:08 -0300

    :sparkles: feat(insights): implements the filter_by_salary_range function

--- src/insights.py
+++ src/insights.py
@@ -72,18 +72,12 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
+    filtered_jobs = []
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                filtered_jobs.append(job)
+        except ValueError:
+            print("One or more parameters are incorrect. Unable to add job.")
+    return filtered_jobs
