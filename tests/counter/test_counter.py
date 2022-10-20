566891f8e0c924a239a4efae3702bc562ab2f0d2
Lalá Nametala <lalanametala@gmail.com>
2022-10-19 23:08:59 -0300

    :test_tube: test(sorting): implements the test_sort_by_criteria function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,10 +2,10 @@ from src.counter import count_ocurrences
 
 
 def test_counter():
-    WORDS = ["python", "PYTHON", "javascript", "JAVAscript"]
-    PATH = "src/jobs.csv"
+    words = ["python", "PYTHON", "javascript", "JAVAscript"]
+    path = "src/jobs.csv"
     expected_results = [1639, 1639, 122, 122]
 
-    for i in range(len(WORDS)):
-        count_word = count_ocurrences(PATH, WORDS[i])
+    for i in range(len(words)):
+        count_word = count_ocurrences(path, words[i])
         assert count_word == expected_results[i]
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,18 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+from src.jobs import read
+from src.insights import get_min_salary, get_max_salary
 
 
 def test_sort_by_criteria():
-    pass
+    path = "src/jobs.csv"
+    all_jobs = read(path)
+
+    criteria_keys = {
+        "max_salary": get_max_salary,
+        "min_salary": get_min_salary,
+    }
+
+    for key in criteria_keys:
+        sort_by(all_jobs, key)
+        expected = criteria_keys[key](path)
+        assert all_jobs[0] == str(expected)
